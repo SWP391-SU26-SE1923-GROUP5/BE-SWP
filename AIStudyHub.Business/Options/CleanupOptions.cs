cc7b56fb371a2190990381982f9f38f3e4f55d32
duckhynh <phamduchung888888@gmail.com>
2026-06-17 14:48:50 +0700

    feat(tier): add scheduled tier expiration cleanup worker

--- AIStudyHub.Business/Options/CleanupOptions.cs
+++ AIStudyHub.Business/Options/CleanupOptions.cs
@@ -3,4 +3,5 @@ namespace AIStudyHub.Business.Options;
 public sealed class CleanupOptions
 {
     public int UnverifiedAccountRetentionDays { get; init; } = 7;
+    public int TierExpirationCheckIntervalHours { get; init; } = 24;
 }
