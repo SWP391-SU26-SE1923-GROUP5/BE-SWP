f7895a1343c94ab0db32a6679349b39f57f3cf2b
duckhynh <phamduchung888888@gmail.com>
2026-07-03 08:19:46 +0700

    feat(database): add IsOcrApplied property to ApplicationDbContextModelSnapshot

---
+++ AIStudyHub.Data/Migrations/20260701155935_AddIsOcrAppliedToDocument.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AIStudyHub.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsOcrAppliedToDocument : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOcrApplied",
+                table: "Document",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOcrApplied",
+                table: "Document");
+        }
+    }
+}
